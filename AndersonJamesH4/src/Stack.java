2aca92ccaec2240a92209921df726649b4a921d4
Turnt-SNACO <janderson5@albany.edu>
2017-04-09 17:50:39 -0400

    Stack modified from linked list

---
+++ AndersonJamesH4/src/Stack.java
@@ -0,0 +1,70 @@
+
+public class Stack {
+	private Node origin=null;
+	private Node end=null;
+	public Stack(){}
+
+	public void push(Node node) {
+    	if (origin==null){  
+    		origin=node;
+    		end=node;
+    	}else{
+    		node.setNext(origin);
+    		end=origin;
+    		origin=node;
+    	}
+    }
+	public Node push(){
+		Node temp = origin;
+		deleteNode(origin);
+		return temp;
+	}
+	public Node peek(){
+		return origin;
+	}
+	
+	public void deleteStack(){
+		Node current=origin;
+		while(current!=null){
+			current.setPrevious(null);
+			Node temp = current.getNext();
+			current.setNext(null);
+			current=temp;
+		}
+		origin= null;
+		end=null;
+	}
+	
+	public String printForward(){
+		String list="";
+		Node current=origin;
+		while(current.getNext()!=null){
+			list+=current.getContent()+", ";
+    		current=current.getNext();
+    	}
+		list+=current.getContent();
+		return list;
+	}
+	
+	public String printReverse(){
+		String list="";
+		Node current=end;
+		while(current.getPrevious()!=null){
+			list+=current.getContent()+", ";
+    		current=current.getPrevious();
+    	}
+		list+=current.getContent();
+		return list;
+	}
+	
+	private void deleteNode(Node node){
+		if (node!=origin)
+			node.getPrevious().setNext(node.getNext());
+		else
+			origin=node.getNext();
+		if (node!=end)
+			node.getNext().setPrevious(node.getPrevious());
+		else
+			end=node.getPrevious();
+	}
+}
